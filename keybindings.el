186152ab15dcb19e75172c938ca6d4ec8257957c
cskksc <ckoparkar@live.in>
2014-12-05 15:36:23 +0530

    Magit mode keybinding to 'mg'. Some comments.

--- keybindings.el
+++ keybindings.el
@@ -1,3 +1,4 @@
+;; Dont use this
 (global-unset-key (kbd "<up>"))
 (global-unset-key (kbd "<down>"))
 (global-unset-key (kbd "<left>"))
@@ -19,7 +20,11 @@
 
 ;; Extra navigation
 (global-set-key (kbd "C-x g") 'goto-line)
+(global-set-key (kbd "C-x e") 'end-of-buffer)
+(global-set-key (kbd "C-x a") 'beginning-of-buffer)
 
+
+;; Duplicate line
 (defun duplicate-line()
   (interactive)
   (move-beginning-of-line 1)
@@ -31,8 +36,51 @@
 
 (global-set-key (kbd "C-x d") 'duplicate-line)
 
-(global-set-key (kbd "C-x e") 'end-of-buffer)
-(global-set-key (kbd "C-x a") 'beginning-of-buffer)
+;;;; Multiple Cursors ;;;;;
+
+(global-set-key (kbd "M->") 'mc/mark-next-like-this)
+(global-set-key (kbd "M-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-x .") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-x /") 'mc/edit-lines)
+
+(global-set-key (kbd "M-.") 'mc/unmark-next-like-this)
+(global-set-key (kbd "M-,") 'mc/unmark-previous-like-this)
+
+
+;; iy-go-to-char
+
+(key-chord-define-global "fg" 'iy-go-to-char)
+(key-chord-define-global "df" 'iy-go-to-char-backward)
+
+;; ace-jump
+(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
+
+;; Magit
+(key-chord-define-global "mg" 'magit-status)
+
+;;;; Commenting stuff
+
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+        If no region is selected and current line is not blank and we are not at the end of the line, then comment current line.
+        Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+	  (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+	(comment-dwim arg)))
+
+(global-set-key (kbd "M-;") 'comment-dwim-line)
+
+(global-set-key (kbd "C-x ;") 'comment-or-uncomment-region)
+
+;;; smaertparens traverse
+
+(global-set-key (kbd "M-]") 'sp-next-sexp)
+(global-set-key (kbd "M-[") 'sp-previous-sexp)
+
+(global-set-key (kbd "M-}") 'sp-down-sexp)
+(global-set-key (kbd "M-{") 'sp-up-sexp)
 
 ;; Enable smex, enhancement for M-x
 (global-set-key (kbd "M-x")
@@ -66,46 +114,3 @@
 					 (smex-initialize))
 				  (global-set-key (kbd "C-x C-m") 'smex)
 				  (smex)))
-
-
-;;;; Multiple Cursors ;;;;;
-
-(global-set-key (kbd "M->") 'mc/mark-next-like-this)
-(global-set-key (kbd "M-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-x .") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-x /") 'mc/edit-lines)
-
-(global-set-key (kbd "M-.") 'mc/unmark-next-like-this)
-(global-set-key (kbd "M-,") 'mc/unmark-previous-like-this)
-
-
-;;;;;;;;;
-
-(key-chord-define-global "fg" 'iy-go-to-char)
-(key-chord-define-global "df" 'iy-go-to-char-backward)
-
-(global-set-key (kbd "C-c SPC") 'ace-jump-mode)
-
-;;;; Commenting stuff
-
-(defun comment-dwim-line (&optional arg)
-  "Replacement for the comment-dwim command.
-        If no region is selected and current line is not blank and we are not at the end of the line, then comment current line.
-        Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
-  (interactive "*P")
-  (comment-normalize-vars)
-  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
-	  (comment-or-uncomment-region (line-beginning-position) (line-end-position))
-	(comment-dwim arg)))
-
-(global-set-key (kbd "M-;") 'comment-dwim-line)
-
-(global-set-key (kbd "C-x ;") 'comment-or-uncomment-region)
-
-;;; smaertparens traverse
-
-(global-set-key (kbd "M-]") 'sp-next-sexp)
-(global-set-key (kbd "M-[") 'sp-previous-sexp)
-
-(global-set-key (kbd "M-}") 'sp-down-sexp)
-(global-set-key (kbd "M-{") 'sp-up-sexp)
