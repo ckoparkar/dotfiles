5520a806c285e3a21830fb0d41dc1713f1a1de4c
cskksc <ckoparkar@live.in>
2015-08-28 09:24:09 +0530

    Cleaner modeline.

--- common-stuff.el
+++ common-stuff.el
@@ -75,6 +75,45 @@
 
 (delete-selection-mode 1)
 
+;; hide minor modes emacs
+(defvar mode-line-cleaner-alist
+  `((auto-complete-mode . " ac")
+	(projectile-mode " pm")
+	(eldoc-mode . "")
+	(abbrev-mode . "")
+	(undo-tree-mode . "")
+	(highlight-parentheses-mode . "")
+	(magit-auto-revert-mode . "")
+	(guide-key-mode . "")
+	(smartparens-mode . "")
+
+	;; Major modes
+	(lisp-interaction-mode . "λ")
+	(clojure-mode . "λ")
+	(haskell-mode . "λ")
+	(python-mode . "Py")
+	(emacs-lisp-mode . "λ"))
+  "Alist for `clean-mode-line'.
+
+When you add a new element to the alist, keep in mind that you
+must pass the correct minor/major mode symbol and a string you
+want to use in the modeline *in lieu of* the original.")
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for cleaner in mode-line-cleaner-alist
+		do (let* ((mode (car cleaner))
+				 (mode-str (cdr cleaner))
+				 (old-mode-str (cdr (assq mode minor-mode-alist))))
+			 (when old-mode-str
+				 (setcar old-mode-str mode-str))
+			   ;; major mode
+			 (when (eq mode major-mode)
+			   (setq mode-name mode-str)))))
+
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
 ;; responsible white space
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
