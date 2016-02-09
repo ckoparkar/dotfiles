73f0e7c6759187f38108e234ba11dc0d99e39ab0
cskksc <ckoparkar@live.in>
2016-02-09 15:12:52 +0530

    Add google and swap windows functions.

--- keybindings.el
+++ keybindings.el
@@ -77,6 +77,40 @@
 
 (global-set-key (kbd "C-x ;") 'comment-or-uncomment-region)
 
+(defun google (arg)
+  "Googles a query or region if any.
+With prefix argument, wrap search query in quotes."
+  (interactive "P")
+  (let ((query
+         (if (region-active-p)
+             (buffer-substring (region-beginning) (region-end))
+           (read-string "Google: "))))
+    (when arg (setq query (concat "\"" query "\"")))
+    (browse-url
+     (concat "http://www.google.com/search?ie=utf-8&oe=utf-8&q=" query))))
+
+(global-set-key (kbd "C-c C-g") 'google)
+
+(defun swap-windows ()
+  "If you have 2 windows, it swaps them."
+  (interactive)
+  (cond ((/= (count-windows) 2)
+         (message "You need exactly 2 windows to do this."))
+        (t
+         (let* ((w1 (first (window-list)))
+                (w2 (second (window-list)))
+                (b1 (window-buffer w1))
+                (b2 (window-buffer w2))
+                (s1 (window-start w1))
+                (s2 (window-start w2)))
+           (set-window-buffer w1 b2)
+           (set-window-buffer w2 b1)
+           (set-window-start w1 s2)
+           (set-window-start w2 s1))))
+  (other-window 1))
+
+(global-set-key (kbd "C-c C-s") 'swap-windows)
+
 ;;; smaertparens traverse
 
 (global-set-key (kbd "M-]") 'sp-next-sexp)
