6effec82c6a4d459cc40c7af1ac1c9140aa8ef98
cskksc <ckoparkar@live.in>
2016-04-14 14:01:06 +0530

    Remove cider deprecated fn.

--- .emacs.d/common-stuff.el
+++ .emacs.d/common-stuff.el
@@ -188,7 +188,7 @@
 (use-package cider
   :config
   (progn
-    (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+    (add-hook 'cider-mode-hook 'eldoc-mode)
     (add-hook 'cider-repl-mode-hook #'eldoc-mode)
     (setq nrepl-hide-special-buffers t)
     (setq cider-repl-pop-to-buffer-on-connect nil)
